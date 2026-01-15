04233d92be67cd249887eca69c2a7b607344ab1f
admin <164741640+adminlby@users.noreply.github.com>
2026-01-15 23:29:29 +0800

    Initial commit: MC-OmniSight plugin

---
+++ src/main/java/org/lbynb/mCOmniSight/MCOmniSight.java
@@ -0,0 +1,17 @@
+package org.lbynb.mCOmniSight;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCOmniSight extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
